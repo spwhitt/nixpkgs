e5056be2f12bde8eca4314ee3bbf320984741605
Spencer Whitt <sw@swhitt.me>
2015-04-08 19:43:51 -0400

    Spencer's tweaks: works on Darwin

--- pkgs/development/libraries/gsm/default.nix
+++ pkgs/development/libraries/gsm/default.nix
@@ -4,7 +4,7 @@
 
 let
   inherit (stdenv) isCygwin isDarwin;
-  inherit (stdenv.lib) optional;
+  inherit (stdenv.lib) optional optionalString;
 in
 
 stdenv.mkDerivation rec {
@@ -16,28 +16,26 @@ stdenv.mkDerivation rec {
     sha256 = "0b1mx69jq88wva3wk0hi6fcl5a52qhnq2f9p3f3jdh5k61ma252q";
   };
 
-  patchPhase = [ ''
+  patchPhase = ''
     # Fix include directory
     sed -e 's,$(GSM_INSTALL_ROOT)/inc,$(GSM_INSTALL_ROOT)/include/gsm,' -i Makefile
-  '' ] ++ (if staticSupport then [ ] else
-    (if isDarwin then [ ''
-      # Build dylib on Darwin
-      sed -e 's,libgsm.a,libgsm.dylib,' -i Makefile
-      sed -e 's,$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS),$(LD) -o $(LIBGSM) -dynamiclib -install_name $(GSM_INSTALL_ROOT)/$(LIBGSM) $(GSM_OBJECTS) -lc,' -i Makefile
-    '' ] else [ ''
-      # Build ELF shared object by default
-      sed -e 's,libgsm.a,libgsm.so,' -i Makefile
-      sed -e 's/$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS)/$(LD) -shared -Wl,-soname,libgsm.so -o $(LIBGSM) $(GSM_OBJECTS) -lc/' -i Makefile
-    '' ] ++ [ ''
-      # Hack to add cflags for shared libraries
-      sed -e 's,-c -O2 -DNeedFunctionPrototypes=1,-c -O2 -DNeedFunctionPrototypes=1 ${NIX_CFLAGS_COMPILE},' -i Makefile
-      # Remove line that is unused when building shared libraries
-      sed -e 's,$(RANLIB) $(LIBGSM),,' -i Makefile
-    '' ])
-  );
+  ''
+  + optionalString (!staticSupport) ''
+    # Remove line that is unused when building shared libraries
+    sed -e 's,$(RANLIB) $(LIBGSM),,' -i Makefile
+  ''
+  + optionalString ((!staticSupport) && isDarwin) ''
+    # Build dylib on Darwin
+    sed -e 's,libgsm.a,libgsm.dylib,' -i Makefile
+    sed -e 's,$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS),$(LD) -o $(LIBGSM) -dynamiclib -install_name $(GSM_INSTALL_ROOT)/$(LIBGSM) $(GSM_OBJECTS) -lc,' -i Makefile
+  ''
+  + optionalString ((!staticSupport) && (!isDarwin)) ''
+    # Build ELF shared object by default
+    sed -e 's,libgsm.a,libgsm.so,' -i Makefile
+    sed -e 's/$(AR) $(ARFLAGS) $(LIBGSM) $(GSM_OBJECTS)/$(LD) -shared -Wl,-soname,libgsm.so -o $(LIBGSM) $(GSM_OBJECTS) -lc/' -i Makefile
+  '';
 
   makeFlags = [
-    "SHELL=${stdenv.shell}"
     "INSTALL_ROOT=$(out)"
   ] ++ optional (stdenv.cc.cc.isClang or false) "CC=clang";
 
