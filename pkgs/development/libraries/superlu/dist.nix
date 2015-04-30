ce4723ecfc3a5b59f34843511aa7a00282ae4f66
Spencer Whitt <sw@swhitt.me>
2015-04-30 11:09:15 -0400

    superlu_dist: Linux only for now

--- pkgs/development/libraries/superlu/dist.nix
+++ pkgs/development/libraries/superlu/dist.nix
@@ -1,8 +1,10 @@
-{ callPackage, ... } @ args:
+{ callPackage, lib, ... } @ args:
 
 callPackage ./generic.nix ( args // {
   name = "superlu_dist";
   version = "4.0";
   hash = "09s6r86akm7z41151yrdvlar2iix5py7vyj4fvaab6qp44djn5iq";
   meta.description = "For distributed memory machines";
+  # Does not build on Darwin for some reason
+  meta.platforms = lib.platforms.linux;
 })
